dbbbae77c8de7501491529da77de1f358b94464c
qwezd <qwezd8074@gmail.com>
2025-09-29 17:24:02 +0900

    1

---
+++ src/main/java/com/example/AuthFlow/AuthFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.AuthFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AuthFlow/AuthFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
