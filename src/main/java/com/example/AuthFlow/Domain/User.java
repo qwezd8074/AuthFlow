196c54f4dd5cb0c2e4b24fff8f47d4ea0724ecf4
Duhoon <eclip6@naver.com>
2025-09-30 17:39:38 +0900

    fix: Lombok Getter Setter 이용

--- src/main/java/com/example/AuthFlow/Domain/User.java
+++ src/main/java/com/example/AuthFlow/Domain/User.java
@@ -1,20 +1,25 @@
 package com.example.AuthFlow.Domain;
 
 import jakarta.persistence.*;
-
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.Locale;
 
 @Entity
 @Table(name = "User")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter()
+    @Setter()
     private Long id;
 
+    @Getter()
+    @Setter()
     private String name;
+
+    @Getter()
+    @Setter()
     private String pass;
 
     @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
@@ -22,28 +27,4 @@ public class User {
 
     @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
     private LocalDateTime updatedAt;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPass() {
-        return pass;
-    }
-
-    public void setPass(String pass) {
-        this.pass = pass;
-    }
 }
