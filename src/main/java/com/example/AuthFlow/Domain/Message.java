638d6ac7f3766e96c1bf2587326c2628c8244681
Duhoon <eclip6@naver.com>
2025-10-03 19:23:03 +0900

    feat: Message 도메인 빌드 패턴 적용 및 저장 로직 구현

--- src/main/java/com/example/AuthFlow/Domain/Message.java
+++ src/main/java/com/example/AuthFlow/Domain/Message.java
@@ -3,8 +3,7 @@ package com.example.AuthFlow.Domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -13,37 +12,33 @@ import java.util.UUID;
 
 @Entity
 @Table(name="message")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Message {
     @Id
-    final String id;
+    private String id;
 
     // 메시지 보내는 유저
-    @Getter()
-    @Setter()
     private Long from;
 
     // 메시지 받는 유저
-    @Getter()
-    @Setter()
     private Long to;
 
     // 컨텐츠
-    @Getter()
-    @Setter()
     private String content;
 
-    @Getter()
-    final LocalDateTime createdAt;
+    private LocalDateTime createdAt;
 
-    Message(Long from, Long to, String content){
-        this.id = this.createId();
+    @Builder
+    private Message(Long from, Long to, String content){
+        this.id = this.generateId();
         this.from = from;
         this.to = to;
         this.content = content;
         this.createdAt = LocalDateTime.now();
     }
 
-    private String createId(){
+    private String generateId(){
         long currentDate = LocalDateTime
                 .now()
                 .atZone(ZoneId.systemDefault())
