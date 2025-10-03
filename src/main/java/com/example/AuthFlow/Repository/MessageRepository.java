fb1bbcba08c25da5fe5190a9aacc17ae9d05a4d1
Duhoon <eclip6@naver.com>
2025-10-03 13:58:44 +0900

    feat: message 도메인 및 레포 생성

---
+++ src/main/java/com/example/AuthFlow/Repository/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.example.AuthFlow.Repository;
+
+import com.example.AuthFlow.Domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, String> {
+}
