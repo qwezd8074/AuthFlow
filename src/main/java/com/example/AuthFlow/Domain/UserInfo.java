6b89ed3deab0af990c9978caff743dddcc504888
Duhoon <eclip6@naver.com>
2025-10-04 08:01:48 +0900

    feat: UserInfo 도메인 및 레포 추가

---
+++ src/main/java/com/example/AuthFlow/Domain/UserInfo.java
@@ -0,0 +1,34 @@
+package com.example.AuthFlow.Domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name= "user_info")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private boolean sex;
+    private LocalDateTime birth;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Builder
+    protected UserInfo(Long id, boolean sex, LocalDateTime birth) {
+        this.id = id;
+        this.sex = sex;
+        this.birth = birth;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+}
