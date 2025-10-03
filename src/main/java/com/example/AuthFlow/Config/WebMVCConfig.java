cbde583bb3b6055d77e92676135c29090e30c2a8
Duhoon <eclip6@naver.com>
2025-10-04 07:42:54 +0900

    refactor: MVC Config 디렉터리 수정

--- src/main/java/com/example/AuthFlow/Config/WebMVCConfig.java
+++ src/main/java/com/example/AuthFlow/Config/WebMVCConfig.java
@@ -1,11 +1,11 @@
-package com.example.AuthFlow;
+package com.example.AuthFlow.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Config implements WebMvcConfigurer {
+public class WebMVCConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
