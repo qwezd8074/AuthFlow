9ded1a28a575f3c0e1fb8f595c664f1f444e18db
Duhoon <eclip6@naver.com>
2025-10-08 21:09:13 +0900

    feat: 게임 방 참여 및 나가기 기능 구현

--- src/main/java/com/example/AuthFlow/Handler/GameWebSocketHandler.java
+++ src/main/java/com/example/AuthFlow/Handler/GameWebSocketHandler.java
@@ -1,32 +1,27 @@
 package com.example.AuthFlow.Handler;
 
 import com.example.AuthFlow.DTO.Chat.ChatMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.*;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 public class GameWebSocketHandler implements WebSocketHandler {
-    // key값: session ID - UUID 형식
-    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    // session ID가 이미 할당되어 있는지 확인하기 위한 값
+    private final Map<String, String> sessionRoomId = new ConcurrentHashMap<>();
+
+    // key값: RoomID - 정수(ex. 1, 2, 3) -> key값: sessionID - UUID 형식, val 값: RoomID
+    private final Map<String, Set<WebSocketSession>> roomSessions = new ConcurrentHashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        var sessionId = session.getId();
-        sessions.put(sessionId, session);
-
-        ChatMessage message = ChatMessage.builder().sender(sessionId).receiver("all").build();
-        message.newConnect();
-
-        sessions.values().forEach(s -> {
-            try {
-                if(!s.getId().equals(sessionId)) {
-                    s.sendMessage(new TextMessage(message.getContent()));
-                }
-            } catch (Exception e) {
-                // TODO: throw;
-            }
-        });
+        var str = String.format("session 연결: %s", session.getId());
+        log.info(str);
     }
 
     @Override
@@ -38,16 +33,6 @@ public class GameWebSocketHandler implements WebSocketHandler {
                 .receiver("all")
                 .content(message.getPayload().toString())
                 .build();
-
-        sessions.values().forEach(s -> {
-            try {
-                if(!s.getId().equals(sessionId)) {
-                    s.sendMessage(new TextMessage(msg.getContent()));
-                }
-            } catch (Exception e) {
-                // TODO: throw;
-            }
-        });
     }
 
     @Override
@@ -63,5 +48,29 @@ public class GameWebSocketHandler implements WebSocketHandler {
     @Override
     public boolean supportsPartialMessages() {
         return false;
+    }
+
+    private void join(String groupId, WebSocketSession session) {
+        roomSessions
+                .computeIfAbsent(groupId, key -> ConcurrentHashMap.newKeySet())
+                .add(session);
+
+        sessionRoomId.put(session.getId(), groupId);
+    }
+
+    private void exit(WebSocketSession session) {
+        var groupId = sessionRoomId.get(session.getId());
+
+        if (groupId == null) {
+            throw new NoSuchElementException();
+        }
+
+        roomSessions.computeIfPresent(
+                groupId,
+                (key, innerSession) -> {
+                    innerSession.remove(session);
+                    return innerSession;
+                }
+        );
     }
 }
